505b43cfb45eb86878ca284fc718da85ef1d1442
sgryt <christensen.mikkel@gmail.com>
2016-11-06 14:41:24 +0100

    First attempt to disable mobile master page selection.

--- easyIDDemo/App_Start/RouteConfig.cs
+++ easyIDDemo/App_Start/RouteConfig.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
 using Microsoft.AspNet.FriendlyUrls;
+using System.Web.UI;
 
 namespace easyIDDemo
 {
+    public class MyWebFormsFriendlyUrlResolver : Microsoft.AspNet.FriendlyUrls.Resolvers.WebFormsFriendlyUrlResolver
+    {
+        protected override bool TrySetMobileMasterPage(HttpContextBase httpContext, Page page, String mobileSuffix)
+        {
+            if (mobileSuffix == "Mobile")
+            {
+                return false;
+            }
+            else
+            {
+                return base.TrySetMobileMasterPage(httpContext, page, mobileSuffix);
+            }
+        }
+    }
+
     public static class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
-            routes.EnableFriendlyUrls(settings);
+            routes.EnableFriendlyUrls(settings, new MyWebFormsFriendlyUrlResolver());
         }
     }
 }
