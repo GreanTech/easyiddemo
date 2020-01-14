4c15b5cabfead32d007ccd1a2e546b99335b3c82
Mikkel Christensen <christensen.mikkel@gmail.com>
2020-01-14 11:38:27 +0100

    Added new files that got left out of last commit.

---
+++ easyIDDemo/SSO.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SSO.aspx.cs" Inherits="easyIDDemo.SSO" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <script type="text/javascript" src="Scripts/userLoggedInListener.js"></script>
+    <iframe src="/Login" id="verify-sso" title="Criipto-Verify-SSO" 
+        allowfullscreen="true" scrolling="no" frameborder="0" class="hidden-frame">
+    </iframe>
+</asp:Content>
---
+++ easyIDDemo/SSO.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IdentityModel.Services;
+
+namespace easyIDDemo
+{
+    public partial class SSO : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (User != null && User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                FederatedAuthentication.WSFederationAuthenticationModule.SignOut(true);                
+            }
+            else
+            {
+                Response.Redirect("/");
+            }
+        }
+    }
+}
